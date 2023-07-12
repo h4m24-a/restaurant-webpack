2ca997cbb11a3334eae28b8b24348ecaf7ee1d57
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-07-12 22:58:27 +0100

    added new div to the clear content function

--- src/tabs.js
+++ src/tabs.js
@@ -1,74 +1,69 @@
 import createMenuPage from "./menu";
 import createRestaurantHomePage from "./restaurant";
 
-
-
 const createTabs = () => {
-    const content = document.querySelector("#content");
-    const navDiv = document.createElement("div");
-    navDiv.classList.add("navDiv");
-    
-    const navbar = document.createElement("div");
-    navbar.setAttribute("id", "navbar");
-    
-    // Tabs
-    const logo = document.createElement("img");
-    const div1 = document.createElement("div");
-    const div2 = document.createElement("div");
-    
- 
+  const content = document.querySelector("#content");
+  const navDiv = document.createElement("div");
+  navDiv.classList.add("navDiv");
 
+  const navbar = document.createElement("div");
+  navbar.setAttribute("id", "navbar");
 
-    // Setting id's
-    logo.setAttribute("id", "logo")
-    div1.setAttribute("id", "homeButton");
-    div2.setAttribute("id", "menuButton");
+  // Tabs
+  const logo = document.createElement("img");
+  const div1 = document.createElement("div");
+  const div2 = document.createElement("div");
 
+  // Setting id's
+  logo.setAttribute("id", "logo");
+  div1.setAttribute("id", "homeButton");
+  div2.setAttribute("id", "menuButton");
 
+  // Setting classes
+  div1.classList.add("navText");
+  div2.classList.add("navText");
 
-    // Setting classes
-    div1.classList.add("navText");
-    div2.classList.add("navText");
-   
-    
+  // Text of tabs
+  logo.src =
+    "https://t3.ftcdn.net/jpg/04/75/85/06/360_F_475850614_NV7T6VRqGASmv4p3QQwIZoMmDhFXzQVL.jpg";
+  div1.textContent = "Home";
+  div2.textContent = "Menu";
 
-    // Text of tabs
-    logo.src = 'https://t3.ftcdn.net/jpg/04/75/85/06/360_F_475850614_NV7T6VRqGASmv4p3QQwIZoMmDhFXzQVL.jpg';
-    div1.textContent = "Home";
-    div2.textContent = "Menu";
-   
+  navDiv.appendChild(logo);
+  navDiv.appendChild(div1);
+  navDiv.appendChild(div2);
+  content.appendChild(navDiv);
 
-    navDiv.appendChild(logo)
-    navDiv.appendChild(div1);
-    navDiv.appendChild(div2);
-    content.appendChild(navDiv)
-    
-    navbar.appendChild(div1);
-    navbar.appendChild(div2);
-    content.appendChild(navbar)
-    navDiv.appendChild(navbar)
-    
-    div1.addEventListener('click', () => {
-        clearContent()
-        createRestaurantHomePage();
-    })
+  navbar.appendChild(div1);
+  navbar.appendChild(div2);
+  content.appendChild(navbar);
+  navDiv.appendChild(navbar);
 
-    div2.addEventListener('click', () => {
-        clearContent()
-        createMenuPage();
-    })
-    
-}
+  div1.addEventListener("click", () => {
+    clearContent();
+    createRestaurantHomePage();
+  });
+
+  div2.addEventListener("click", () => {
+    clearContent();
+    createMenuPage();
+  });
+};
 
 function clearContent() {
-    const content = document.querySelector("#content");
-    const newDiv = document.querySelector(".page-content");
-    const homeDiv = document.querySelector(".homeDiv");
-    if (newDiv) {
-        content.removeChild(newDiv);
-    }
-    else if (homeDiv) {
-        content.removeChild(homeDiv);
-    }
+  const content = document.querySelector("#content");
+  const newDiv = document.querySelector(".page-content");
+  const homeDiv = document.querySelector(".homeDiv");
+  const topdishDiv = document.querySelector(".topdishDiv");
+  if (newDiv) {
+    content.removeChild(newDiv);
+  }
+  if (homeDiv) {
+    content.removeChild(homeDiv);
+  }
+
+  if (topdishDiv) {
+    content.removeChild(topdishDiv);
+  }
 }
 export default createTabs;
